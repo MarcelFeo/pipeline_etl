1bca7fd3173554692c7b8ef53fffe9e1d21d3e18
MarcelFeo <marcelfeo29@gmail.com>
2024-11-22 20:21:53 -0300

    feat: add html collector

--- src/drivers/http_requester.py
+++ src/drivers/http_requester.py
@@ -4,7 +4,7 @@ import requests
 class HttpRequester:
 
     def __init__(self) -> None:
-        self.__url = 'https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbnhxSThJSmhIdUI3dkxCNm5FT2sxc3JYLUtXd3xBQ3Jtc0ttVHZkRGRGZ3RKTWx2aWhYLW9MeHdHV0hEc0dJVGZzdkJGak1OQTc1ZTRQTGlwdUtjbW10YXAxUDZDYXZEUTNwdEg1TTUySVlzazh1YzZtSVBLYXRVR3R1WGtFWjk3SmdKX3RhS0FjX1lpYUNSMEU1dw&q=https%3A%2F%2Fweb.archive.org%2Fweb%2F20121007172955%2Fhttps%3A%2F%2Fwww.nga.gov%2Fcollection%2FanZ1.htm&v=MXUqKvEST8s'
+        self.__url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
 
     def request_from_page(self) -> Dict[int, str]:
         response = requests.get(self.__url)
--- src/drivers/http_requester_test.py
+++ src/drivers/http_requester_test.py
@@ -1,7 +1,7 @@
 from .http_requester import HttpRequester
 
 def test_request_from_page(requests_mock):
-    url = 'https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbnhxSThJSmhIdUI3dkxCNm5FT2sxc3JYLUtXd3xBQ3Jtc0ttVHZkRGRGZ3RKTWx2aWhYLW9MeHdHV0hEc0dJVGZzdkJGak1OQTc1ZTRQTGlwdUtjbW10YXAxUDZDYXZEUTNwdEg1TTUySVlzazh1YzZtSVBLYXRVR3R1WGtFWjk3SmdKX3RhS0FjX1lpYUNSMEU1dw&q=https%3A%2F%2Fweb.archive.org%2Fweb%2F20121007172955%2Fhttps%3A%2F%2Fwww.nga.gov%2Fcollection%2FanZ1.htm&v=MXUqKvEST8s'
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
     response_context = 'url text mock'
     requests_mock.get(url, status_code=200, text=response_context)
 
